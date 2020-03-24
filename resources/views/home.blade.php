8d26cf45a6de1089e930adf2fc53157f3b9b6b91
TANAKA RYOKO <ryochi0825@yahoo.co.jp>
2020-03-24 08:24:40 +0000

    20200324

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,52 +10,34 @@
     <h1>コンテナ船管理システム</h1>
     <br>
     <br>
-<div class="container">
-    <div class ="row" id="command">
-        <div class="col-md-6">
-        <input type="button" id="button" value="本船情報登録" style="width:300px;height:100px">
-        </div>
-        <div class="col-md-6">
-        <input type="button" id="button" value="動静表更新" style="width:300px;height:100px">
-        </div>
-    </div> 
-    <br>
-    <br>
-    <div class ="row" id="command">
-        <div class="col-md-6">
-        <input type="button" id="button" value="通関注文書作成" style="width:300px;height:100px">
-        </div>
-        <div class="col-md-6">
-        <input type="button" id="button" value="保険申込書作成" style="width:300px;height:100px">
-        </div>
-    </div> 
-    <br>
-    <br>
-    <div class ="row" id="command">
-        <div class="col-md-6">
-        <input type="button" id="button" value="保証票作成" style="width:300px;height:100px">
-        </div>
-        <div class="col-md-6">
-        <input type="button" id="button" value="業務進捗表入力" style="width:300px;height:100px">
-        </div>
-    </div>        
-     
-            
-            
-                <!--<div class="card">-->
-                <!--<div class="card-header">Dashboard</div>-->
-
-                <!--<div class="card-body">-->
-                <!--    @if (session('status'))-->
-                <!--        <div class="alert alert-success" role="alert">-->
-                <!--            {{ session('status') }}-->
-                <!--        </div>-->
-                <!--    @endif-->
-
-                <!--    You are logged in!-->
-                <!--</div>-->
+    <div class="container">
+        <div class ="row" id="command">
+            <div class="col-md-6">
+            <input type="button" id="button" value="本船情報登録" style="width:300px;height:100px">
             </div>
-        </div>
+            <div class="col-md-6">
+            <input type="button" id="button" value="動静表更新" style="width:300px;height:100px">
+            </div>
+        </div> 
+        <br>
+        <br>
+        <div class ="row" id="command">
+            <div class="col-md-6">
+            <input type="button" id="button" value="通関注文書作成" style="width:300px;height:100px">
+            </div>
+            <div class="col-md-6">
+            <input type="button" id="button" value="保険申込書作成" style="width:300px;height:100px">
+            </div>
+        </div> 
+        <br>
+        <br>
+        <div class ="row" id="command">
+            <div class="col-md-6">
+            <input type="button" id="button" value="保証票作成" style="width:300px;height:100px">
+            </div>
+            <div class="col-md-6">
+            <input type="button" id="button" value="業務進捗表入力" style="width:300px;height:100px">
+            </div>
+        </div>        
     </div>
-</div>
 @endsection
