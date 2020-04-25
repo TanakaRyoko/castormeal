26cc88a18274f5e08590e076307a404b6aa770d3
mozy okubo <mozy.okubo@gmail.com>
2020-04-26 04:03:43 +0900

    出報エラー の インポート、エクスポート機能を実装した
    laravel-excelのバージョンも3にあげた

---
+++ app/Imports/ReportsImport.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Imports;
+
+use App\Report;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+
+class ReportsImport implements ToModel, WithHeadingRow
+{
+    /**
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
+    public function model(array $row)
+    {
+        return new Report([
+            'error_no'          => $row['error_no'],
+            'order_no'          => $row['order_no'],
+            'consignee_code'    => $row['consignee_code'],
+            'consignee'         => $row['consignee'],
+            'hub_code'          => $row['hub_code'],
+            'hub_name'          => $row['hub_name'],
+            'product_code'      => $row['product_code'],
+            'product'           => $row['product'],
+            'yoryo'             => $row['yoryo'],
+            'mt'                => $row['mt'],
+            'supply_unit_price' => $row['supply_unit_price'],
+            'supply_price'      => $row['supply_price'],
+            'supply_month'      => $row['supply_month'],
+            'ship_date'         => $row['ship_date'],
+            'supply_delivery_condition'     => $row['supply_delivery_condition'],
+            'recieving_delivery_condition'  => $row['recieving_delivery_condition'],
+            'jibetu'            => $row['jibetu'],
+            'shupou_no'         => $row['shupou_no'],
+            'zaikokubun'        => $row['zaikokubun'],
+            'vessel'            => $row['vessel'],
+            'yamamoto'          => $row['yamamoto'],
+            'shupou_uketuke_date'   => $row['shupou_uketuke_date'],
+            'setuzoku_code'     => $row['setuzoku_code'],
+        ]);
+    }
+}
--- app/Report.php
+++ app/Report.php
@@ -3,49 +3,46 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Report;
 
 class Report extends Model
 {
     protected $fillable = [
-           'error_no',
-           'order_no',
-           'consignee_code',
-           'consignee',
-           'hub_code',
-           'hub_name',
-           'product_code',
-           'product',
-           'yoryo',
-           'mt',
-           'supply_unit_price',
-           'supply_price',
-           'supply_month',
-           'ship_date',
-           'supply_delivery_condition',
-           'recieving_delivery_condition',
-           'jibetu',
-           'shupou_no',
-           'zaikokubun',
-           'vessel',
-           'yamamoto',
-           'shupou_uketuke_date',
-           'setuzoku_code',
-           'error_code',
-           'error_message'
-        
+        'error_no',
+        'order_no',
+        'consignee_code',
+        'consignee',
+        'hub_code',
+        'hub_name',
+        'product_code',
+        'product',
+        'yoryo',
+        'mt',
+        'supply_unit_price',
+        'supply_price',
+        'supply_month',
+        'ship_date',
+        'supply_delivery_condition',
+        'recieving_delivery_condition',
+        'jibetu',
+        'shupou_no',
+        'zaikokubun',
+        'vessel',
+        'yamamoto',
+        'shupou_uketuke_date',
+        'setuzoku_code',
+        'error_code',
+        'error_message'
+
     ];
-    
+
     public function consignees()
     {
         return $this->hasMany('App\Consignee');
     }
-    
-     //hasManyの設定
+
+    //hasManyの設定
     public function products()
     {
         return $this->hasMany('App\Product');
     }
-    
-    
 }
