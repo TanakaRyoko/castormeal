5629f437dea88a400785f96a9378809ffb0721eb
mozy okubo <mozy.okubo@gmail.com>
2020-05-09 13:45:07 +0900

    fix vessel export

--- app/Exports/VesselExport.php
+++ app/Exports/VesselExport.php
@@ -13,34 +13,30 @@ class VesselExport implements FromCollection, WithHeadings
      */
     public function collection()
     {
-        $vessel_data = Vessel::all();
+        return Vessel::all();
     }
 
     public function headings(): array
     {
-        foreach($vessel_data as $vessel)
-     {
-      $vessel_array[] = array(
-       'contract_no'  => $vessel->contract_no,
-       'product'   => $vessel->product,
-       'shipping_company'    => $vessel->shipping_company,
-       'port_of_discharging'  => $vessel->port_of_discharging,
-       'estimate_time_of_loading'   => $vessel->estimate_time_of_loading,
-       'time_of_arrival'   => $vessel->time_of_arrival,
-       'containers'   => $vessel->containers,
-       'vessel_no'   => $vessel->vessel_no,
-       'register_no'   => $vessel->register_no,
-       'mt'   => $vessel->mt,
-       'bl_no'   => $vessel->bl_no,
-       'remmitance'   => $vessel->remmitance,
-       'unit_price'   => $vessel->unit_price,
-       'rate'   => $vessel->rate,
-       'interest_rates'   => $vessel->interest_rates,
-       'japanese_yen'   => $vessel->japanese_yen,
-       'bank_charge'   => $vessel->bank_charge
-       );
-      
-     }
-    
+        return [
+            'id',
+            'contract_no',
+            'product',
+            'shipping_company',
+            'port_of_discharging',
+            'estimate_time_of_loading',
+            'time_of_arrival',
+            'containers',
+            'vessel_no',
+            'register_no',
+            'mt',
+            'bl_no',
+            'remmitance',
+            'unit_price',
+            'rate',
+            'interest_rates',
+            'japanese_yen',
+            'bank_charge',
+        ];
     }
 }
